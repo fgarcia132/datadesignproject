a78a8ac273daefadf0d10e3bc88a6c10ee6d5d68
Francisco Garcia <ciscog11@msn.com>
2016-01-14 16:44:05 -0700

    added s to review

--- steamreviews.php
+++ steamreviews.php
@@ -4,7 +4,7 @@
 			<title>Steam Reviews</title>
 		</head>
 	<body>
-		<h1>Steam Review Data Relationship Model</h1>
+		<h1>Steam Reviews Data Relationship Model</h1>
 		<img src="datadesignproject.svg" />
 	</body>
 </html>
