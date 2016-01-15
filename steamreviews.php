e9e1d12afa00bbed442ee9db618cb9e5986cdc5a
Francisco Garcia <ciscog11@msn.com>
2016-01-15 08:02:30 -0700

    added persona

--- steamreviews.php
+++ steamreviews.php
@@ -6,5 +6,7 @@
 	<body>
 		<h1>Steam Reviews Data Relationship Model</h1>
 		<img src="datadesignproject.svg" />
+	<h2>Persona</h2>
+		<p1>The persona for Steam Reviews is someone that would like to share their experience with a certain game. Stating their opinion on whether they enjoyed the game or did not enjoy the game. Conversely that same person will read other peoples' reviews to forumulate their own opinion on a game that they may or may not buy.</p1>
 	</body>
 </html>
